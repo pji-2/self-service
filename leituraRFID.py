62e875029ce14db78b1c2216595b81719a7af7fa
camillabarreto <camillabarretodesousa@gmail.com>
2019-11-19 17:50:36 -0300

    leituraRFID.py add no master

--- leituraRFID.py
+++ leituraRFID.py
@@ -1,5 +1,8 @@
 import serial
 
+# teste
+
+
 def leituraRFID():
     tag_list = list()
 
@@ -15,9 +18,25 @@ def leituraRFID():
         tag = bytearray()
         tag = ser.readline(max_bytes)
 
-        if not tag_list.__contains__(tag):
+        print(tag)
+        tag = tag.decode("utf-8", "ignore")
+        print(tag)
+        tag = tag.split("\r\n")[0]
+
+        try:
+            print(tag)
+            tag = tag.split("\x03")[1]
+        except:
+            pass
+        try:
+            print(tag)
+            tag = tag.split("\x02")[1]
+        except:
+            pass
+        if len(tag) > 0 and not tag_list.__contains__(tag):
             tag_list.append(tag)
             print(tag_list)
             # busco o produto no banco de dados
             # se produto existir > exibir produto em tela
             # se nao > exibir mensagem de erro ~OU~ nao exibir nada
+
