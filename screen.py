b781b63811da82290cc081db560d985390ab300b
saromtorres <saromtorres@gmail.com>
2019-11-27 19:41:56 -0200

    comunicacao rfid com tela

--- screen.py
+++ screen.py
@@ -5,7 +5,36 @@ import pathlib
 import sys
 import serial
 import os
+from firebase import firebase
+from firebase.firebase import FirebaseAuthentication, FirebaseApplication
+from database import listProduct, autenticacao, pagamento, getClient
 from gi.repository import GLib
+
+authentication = firebase.FirebaseAuthentication(
+    'oiPMNklhLclGiEpFGzVmOv6AMsV6KOQSadfTLAap', 'elisa.rodrigues444@gmail.com', True, True)
+firebase = FirebaseApplication(
+    'https://pji2-ade1a.firebaseio.com', authentication)
+
+listTag = ['2200D879C9','2200D879CA','2200D879C4','2200D879C8','2200D879C7']
+lista = listProduct(listTag,firebase)
+class Inicio:
+    def __init__(self,arq):
+        self.bd = Gtk.Builder()
+        self.bd.add_from_file(arq)
+        self.tela = self.bd.get_object('inicioWin')
+        self.tela.show_all()
+        self.botaoConfirmar = self.bd.get_object('botaoIniciar')
+        self.bd.connect_signals({"btIniciar_click": self.btIniciar_click, "quit_win": self.quit})
+    
+    def btIniciar_click(self,obj,data=None):
+        path = pathlib.Path(sys.argv[0])
+        path = str(path.parent)
+        tela = Tela('%s/tela.glade' % path)
+        rfid = RFID()
+        
+   
+    def quit(self,*args):
+            Gtk.main_quit()
 
 class Tela:
     def __init__(self,arq):
@@ -14,19 +43,17 @@ class Tela:
         self.tela = self.bd.get_object('tela')
         self.tela.show_all()
         self.botaoCancelar = self.bd.get_object('botaoCancelar')
-        self.botaoCancelar.show()
         self.botaoConfirmar = self.bd.get_object('botaoConfirmar')
-        self.botaoCancelar.show()
         self.labelTexto = self.bd.get_object('labelTexto')
-        self.labelTexto.show()
         self.bd.connect_signals({"btConfirmar_click": self.btConfirmar_click, "quit_win": self.quit})
 
     def btConfirmar_click(self,obj,data=None):
         path = pathlib.Path(sys.argv[0])
         path = str(path.parent)
         auth = AutenticacaoTela('%s/autenticacao.glade' % path)
-        #self.labelTexto.set_text("Hello World")
-        #pass
+
+    def mostraProduto(self,tag,*args):
+        self.labelTexto.set_text(tag)
 
     def quit(self,*args):
         Gtk.main_quit()
@@ -38,74 +65,86 @@ class AutenticacaoTela:
         self.authWin = self.builder.get_object('authWin')
         self.authWin.show_all()
         self.nomeEntry = self.builder.get_object('nomeEntry')
-        #self.nomeEntry.show()
-        self.senhaEntry=self.builder.get_object('senhaEntry')
-        #self.senhaEntry.show()
+        self.senhaEntry = self.builder.get_object('senhaEntry')
         self.botaoOk = self.builder.get_object('botaoOK')
-        #self.botaoOk.show()
         self.botaoCancelar = self.builder.get_object('botaoCancelar')
-        #self.botaoCancelar.show()
-        self.builder.connect_signals({"quit_win": self.quit})
+        self.builder.connect_signals({"quit_win": self.quit,"autenticar_click":self.autenticar})
 
     def quit(self,*args):
-        Gtk.quit()
+        Gtk.main_quit()
 
+    def autenticar(self,*args):
+        nome = self.nomeEntry.get_text()
+        senha = self.senhaEntry.get_text()
+        
+        user = getClient(nome,firebase)
+        if autenticacao(nome,senha,firebase):
+            path = pathlib.Path(sys.argv[0])
+            path = str(path.parent)
+            pagTela = PagTela('%s/pagamento.glade' % path,user['nome'],user['saldo'])
+        else:
+            self.nomeEntry.set_text('Inválido. Digite novamente')
+            self.senhaEntry.set_text('Inválido. Digite novamente')
 
-# class RFID:
+class PagTela:
+    def __init__(self,arq,userNome,userSaldo):
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file(arq)
+        self.dialogPag = self.builder.get_object('dialogPag')
+        self.dialogPag.show_all()
+        self.botaoSim = self.builder.get_object('botaoSim')
+        self.botaoNao = self.builder.get_object('botaoNao')
+        self.labelPag = self.builder.get_object('labelPagamento')
+        text = userNome + " seu saldo é de R$" + str(userSaldo) + ".\n Deseja confirmar o pagamento?"
+        self.labelPag.set_text(text)
+        self.builder.connect_signals({"quit_win": self.quit})
     
-#     def __init__(self):
-#         self.tag_list = list()
+    def quit(self,*args):
+        Gtk.main_quit()
 
-#         # configuração da porta serial
-#         self.serial_port = '/dev/ttyUSB0'
-#         self.port_speed = 9600
-#         self.max_bytes = -1
-#         self.ser = serial.Serial(self.serial_port, self.port_speed, timeout=3)
-# #------------------------------------------------------------------
-#         self.channel = Glib.IOChannel(self.ser)
-#         self.channel.set_flags(Glib.IO_FLAG_NONBLOCK)
+class RFID:
+    
+    def __init__(self,tela):
+        self.tag_list = list()
 
-#         self.cond = Glib.IOCondition(Glib.IOCondition.IN)
-#         self.channel.add_watch(self.cond,self.printList)
+        # configuração da porta serial
+        self.serial_port = '/dev/ttyUSB0'
+        self.port_speed = 9600
+        self.max_bytes = -1
+        self.ser = serial.Serial(self.serial_port, self.port_speed, timeout=3)
+        self.tela = tela
+#------------------------------------------------------------------
+        self.channel = GLib.IOChannel(self.ser.fileno())
+        self.channel.set_flags(GLib.IO_FLAG_NONBLOCK)
+
+        self.cond = GLib.IOCondition(GLib.IOCondition.IN)
+        self.channel.add_watch(self.cond,self.leituraRFID)
 
 # #-----------------------------------------------------------------
 
-#     def leituraRFID(self):
-#         while True:##
-#             # ler somente uma linha (sequencia de bytes)
-#             print('lendo:')
-#             tag = bytearray()
-#             tag = self.ser.readline(self.max_bytes)
-
-            
-#  def printList():
-#     print(tag)
-#     tag = tag.decode("utf-8", "ignore")
-#     print(tag)
-#     tag = tag.split("\r\n")[0]
-
-#     try:
-#         print(tag)
-#         tag = tag.split("\x03")[1]
-#     except:
-#         pass
-#     try:
-#         print(tag)
-#         tag = tag.split("\x02")[1]
-#     except:
-#         pass
-#     if len(tag) > 0 and not self.tag_list.__contains__(tag):
-#         self.tag_list.append(tag)
-#         print(self.tag_list)
-#         return tag
-
+    def leituraRFID(self,*args):
+            tag = bytearray()
+            tag = self.ser.readline(self.max_bytes)
+            tag = tag.decode("utf-8", "ignore")
+            tag = tag.split("\r\n")[0]
+            try:
+                tag = tag.split("\x03")[1]
+            except:
+                pass
+            try:
+                tag = tag.split("\x02")[1]
+            except:
+                pass
+            if len(tag) > 0 and not self.tag_list.__contains__(tag):
+                self.tag_list.append(tag)
+                print(tag)
+            return tag
 
 if __name__ == '__main__':
     path = pathlib.Path(sys.argv[0])
     path = str(path.parent)
-    tela = Tela('%s/tela.glade' % path)
+    inicio = Inicio('%s/inicio.glade' % path)
     Gtk.main()
-    #rfid = RFID()
 
 
 
